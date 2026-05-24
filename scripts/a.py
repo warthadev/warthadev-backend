faa329ad14515fa846bb62ffd27149d4bf6bd0a2
warthadev <59157041+warthadev@users.noreply.github.com>
2026-05-25 03:37:42 +0700

    update scripts/a.py

--- scripts/a.py
+++ scripts/a.py
@@ -1,3 +1,17 @@
-# New File
+# generate_telethon_session.py
+import asyncio
+from telethon import TelegramClient
+from telethon.sessions import StringSession
 
-Created from GitHub Editor
+API_ID = int(input("Enter API ID: "))
+API_HASH = input("Enter API HASH: ")
+
+async def main():
+    async with TelegramClient(StringSession(), API_ID, API_HASH) as client:
+        await client.start()
+        session_str = client.session.save()
+        print("\n=== COPY THIS SESSION STRING ===\n")
+        print(session_str)
+        print("\n================================\n")
+
+asyncio.run(main())
